dd7e1e03cc7664bff2f871b7aefdb3f21003b6ca
blend-zach <99422196+blend-zach@users.noreply.github.com>
2023-01-07 16:46:02 -0500

    Add files via upload

--- rs_review.py
+++ rs_review.py
@@ -108,15 +108,9 @@ def user_input_features():
             data = df_breadcrumb_1
     else:
         data = kw_df
-    h1_selection = st.sidebar.selectbox(label="Selected H1 (required)",
-                                        options=["Choose..."] + data['H1'].unique().tolist())
-    # st.sidebar.header("Configuration")
-    # with st.sidebar.form(key="grid_reset"):
-    #     n_recs = st.slider("Number of Autoship Recommendations", 3, 50, 12)
-    #     n_cols = st.number_input("Number of Recommendations Each Row", 1, 10, 5)
-    #     st.form_submit_button(label="Reset recs and layout")
-    return keyword, category, h1_selection, data #, n_recs, n_cols
-
+    # h1_selection = st.sidebar.selectbox(label="Selected H1 (required)",
+    #                                     options=["Choose..."] + data['H1'].unique().tolist())
+    return keyword, category, [], data
 
 filter_bar = user_input_features()
 keyword_filter = filter_bar[0]
@@ -132,57 +126,91 @@ def make_grid(cols, rows):
             grid[i] = st.columns(rows)
     return grid
 
-
-# Content of the page (NON-AGGRID)
-if h1_selected != 'Choose...' and breadcrumb_filter != []:
-    st.subheader(f"Selected H1: {h1_selected}")
-    st.write(filtered_data[filtered_data['H1'] == h1_selected]['H1 URL'].values[0])
-    for i, row in filtered_data[filtered_data['H1'] == h1_selected].reset_index().iterrows():
-        label = f"{i + 1} - {row['anchor_text']}"
-        with st.expander(label=label, expanded=False):
-            url = row['Related Search URL']
-            breadcrumb = [row['Related Breadcrumb Depth 1'], row['Related Breadcrumb Depth 2'],
-                          row['Related Breadcrumb Depth 3'], row['Related Breadcrumb Depth 4'],
-                          row['Related Breadcrumb Depth 5']]
-            breadcrumb = [x for x in breadcrumb if str(x) != 'nan']
-            mygrid = make_grid(2, 2)
-            mygrid[0][0].write(f"- Breadcrumb: {' -> '.join(breadcrumb)}")
-            mygrid[0][1].write(f"- Similarity: {row['Similarity']}")
-            mygrid[1][0].write(f"- Related URL: {url}")
-            mygrid[1][1].write(f"- Page Type: {row['page_type']}")
-            # st.write(f"{i + 1}. {row['Related H1']}")
-            edit_anchor_text = st.text_input(label="Edit Anchor Text", value='', placeholder=row['Related H1'])
-            if edit_anchor_text:
-                tmp_idx = df[(df['H1'] == h1_selected) & (df['Related H1'] == row['Related H1'])].index[0]
-                df.loc[tmp_idx, 'Edited Related H1'] = edit_anchor_text
-# create some dashboard here
-elif h1_selected == 'Choose...':
+page_length = 12
+# Content of the page
+# home page (display two plots)
+if breadcrumb_filter == [] and keyword_filter == "":
     st.subheader("Overview Status of the Related Search Results")
     a = df.drop_duplicates(subset=['H1'])
     st.write(px.pie(a, names='H1_BC1'))
     x = df['H1'].value_counts().to_frame()
     st.write(px.histogram(x, x="H1"), use_container_width=True)
-# Content of the page (AGGRID)
-# if h1_selected != 'Choose...' and aggrid != 0:
+# after applied filter
+else:
+    h1s = filtered_data['H1'].unique().tolist()
+    page_bar = st.selectbox(label="Page", options=[i for i in range(1, len(h1s) // page_length + 2)])
+    # print(h1s, page_bar, range(1, len(h1s) // page_length + 1))
+    h1_list_on_page = h1s[(page_bar - 1) * page_length: page_bar * page_length]
+    print(h1_list_on_page)
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        a = [st.button(label=h1_list_on_page[i]) for i in range(0, len(h1_list_on_page), 4)]
+    with col2:
+        b = [st.button(label=h1_list_on_page[i]) for i in range(1, len(h1_list_on_page), 4)]
+    with col3:
+        c = [st.button(label=h1_list_on_page[i]) for i in range(2, len(h1_list_on_page), 4)]
+    with col4:
+        d = [st.button(label=h1_list_on_page[i]) for i in range(3, len(h1_list_on_page), 4)]
+    h1_button_on_page = a + b + c + d
+    h1_1 = h1_button_on_page[0: len(h1_button_on_page): 3]
+    h1_2 = h1_button_on_page[1: len(h1_button_on_page): 3]
+    h1_3 = h1_button_on_page[2: len(h1_button_on_page): 3]
+    h1_button_on_page = h1_1 + h1_2 + h1_3
+    # h1_button_on_page = [st.button(label=h1) for h1 in h1_list_on_page]
+    if True in h1_button_on_page:
+        h1_selected = h1_list_on_page[h1_button_on_page.index(True)]
+    else:
+        h1_selected = ''
+    print(h1_selected)
+    if h1_selected != '':
+        st.subheader(f"Selected H1: {h1_selected}")
+        st.write(filtered_data[filtered_data['H1'] == h1_selected]['H1 URL'].values[0])
+        for i, row in filtered_data[filtered_data['H1'] == h1_selected].reset_index().iterrows():
+            label = f"{i + 1} - {row['anchor_text']}"
+            with st.expander(label=label, expanded=False):
+                url = row['Related Search URL']
+                breadcrumb = [row['Related Breadcrumb Depth 1'], row['Related Breadcrumb Depth 2'],
+                              row['Related Breadcrumb Depth 3'], row['Related Breadcrumb Depth 4'],
+                              row['Related Breadcrumb Depth 5']]
+                breadcrumb = [x for x in breadcrumb if str(x) != 'nan']
+                mygrid = make_grid(2, 2)
+                mygrid[0][0].write(f"- Breadcrumb: {' -> '.join(breadcrumb)}")
+                mygrid[0][1].write(f"- Similarity: {row['Similarity']}")
+                mygrid[1][0].write(f"- Related URL: {url}")
+                mygrid[1][1].write(f"- Page Type: {row['page_type']}")
+                edit_anchor_text = st.text_input(label="Edit Anchor Text", value='', placeholder=row['Related H1'])
+                if edit_anchor_text:
+                    tmp_idx = df[(df['H1'] == h1_selected) & (df['Related H1'] == row['Related H1'])].index[0]
+                    df.loc[tmp_idx, 'Edited Related H1'] = edit_anchor_text
+
+
+
+# if h1_selected != 'Choose...' and breadcrumb_filter != []:
 #     st.subheader(f"Selected H1: {h1_selected}")
-#     df_aggrid = filtered_data[filtered_data['H1'] == h1_selected].reset_index()
-#     # show the dataframe using aggrid
-#     gb = GridOptionsBuilder.from_dataframe(df_aggrid)
-#     gb.configure_pagination(paginationPageSize=12)
-#     gb.configure_side_bar()
-#     gb.configure_selection("multiple", use_checkbox=True, groupSelectsChildren="Group checkbox select children")
-#     gb.configure_column("anchor_text", editable=True)
-#     gridOptions = gb.build()
-#     grid_response = AgGrid(
-#         df_aggrid,
-#         gridOptions=gridOptions,
-#         data_return_mode='AS_INPUT',
-#         update_mode='MODEL_CHANGED',
-#         fit_columns_on_grid_load=False,
-#         enable_enterprise_modules=True,
-#         # rowDragEntireRow=True,
-#         height=350,
-#         width='100%',
-#     )
+#     st.write(filtered_data[filtered_data['H1'] == h1_selected]['H1 URL'].values[0])
+#     for i, row in filtered_data[filtered_data['H1'] == h1_selected].reset_index().iterrows():
+#         label = f"{i + 1} - {row['anchor_text']}"
+#         with st.expander(label=label, expanded=False):
+#             url = row['Related Search URL']
+#             breadcrumb = [row['Related Breadcrumb Depth 1'], row['Related Breadcrumb Depth 2'],
+#                           row['Related Breadcrumb Depth 3'], row['Related Breadcrumb Depth 4'],
+#                           row['Related Breadcrumb Depth 5']]
+#             breadcrumb = [x for x in breadcrumb if str(x) != 'nan']
+#             mygrid = make_grid(2, 2)
+#             mygrid[0][0].write(f"- Breadcrumb: {' -> '.join(breadcrumb)}")
+#             mygrid[0][1].write(f"- Similarity: {row['Similarity']}")
+#             mygrid[1][0].write(f"- Related URL: {url}")
+#             mygrid[1][1].write(f"- Page Type: {row['page_type']}")
+#             edit_anchor_text = st.text_input(label="Edit Anchor Text", value='', placeholder=row['Related H1'])
+#             if edit_anchor_text:
+#                 tmp_idx = df[(df['H1'] == h1_selected) & (df['Related H1'] == row['Related H1'])].index[0]
+#                 df.loc[tmp_idx, 'Edited Related H1'] = edit_anchor_text
+# create some dashboard here
+# elif h1_selected == 'Choose...':
+#     st.subheader("Overview Status of the Related Search Results")
+#     a = df.drop_duplicates(subset=['H1'])
+#     st.write(px.pie(a, names='H1_BC1'))
+#     x = df['H1'].value_counts().to_frame()
+#     st.write(px.histogram(x, x="H1"), use_container_width=True)
 
 st.sidebar.download_button('Download Current Dataframe', df.to_csv(), 'related-search-full-run.csv')
